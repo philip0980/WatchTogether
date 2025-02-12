6f0583d44d2a4e62d8981b27105f84a782e94463
Alochan Chaudhary <127869236+philip0980@users.noreply.github.com>
2025-02-13 01:08:46 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -6,13 +6,13 @@ import dotenv from "dotenv";
 
 const app = express();
 const server = http.createServer(app);
-app.use(cors({ origin: "https://localhost:5173" }));
+app.use(cors({ origin: "https://client-8guormrqc-philip0980s-projects.vercel.app/" }));
 
 dotenv.config();
 
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:5173",
+    origin: "https://client-8guormrqc-philip0980s-projects.vercel.app/",
     methods: ["POST", "GET"],
   },
 });
