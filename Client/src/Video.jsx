a5f0e5d1a580b8851c531feff62a19e8227ab8b4
Alochan Chaudhary <127869236+philip0980@users.noreply.github.com>
2025-02-13 00:28:29 +0530

    Update Video.jsx

--- Client/src/Video.jsx
+++ Client/src/Video.jsx
@@ -45,7 +45,7 @@ const Video = () => {
   }, []);
 
   useEffect(() => {
-    socketRef.current = io("https://watch-together-nski.vercel.app");
+    socketRef.current = io("https://watchtogether-jpek.onrender.com/");
 
     socketRef.current.on("connect", () => {
       console.log(
